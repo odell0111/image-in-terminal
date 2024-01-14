6cbcbbdbaa8c00a4104ddc58e4bd2213ec776624
LG-M234H\odell <odellgm11012001@gmail.com>
2024-01-14 07:01:02 -0500

    no message

--- src/imageinterminal/__init__.py
+++ src/imageinterminal/__init__.py
@@ -1,6 +1,6 @@
 import sys
 from pathlib import Path
-path = str(Path(__file__).parent) # image-in-terminal/src/iit
+path = str(Path(__file__).parent) # image-in-terminal/src/imageinterminal
 if path not in sys.path:
   sys.path.append(path)
 
